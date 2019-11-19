21f5076a35c57c22c364f8eda67ea542e0e9c9d3
Taylor <c1171350132@gmail.com>
2019-11-19 17:56:32 +0800

    add deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,9 @@
 module.exports = {
     base: '/Vuepress_blog/',
     title: 'Vuepress_blog',
-    description: 'My vuepress blog'
-    themeConfig: {
-        nav: [
-            { text: '主页', link: '/' },
-            { text: '博文',
-              items: [
-                { text: 'Android', link: '/android/' },
-                { text: 'ios', link: '/ios/' },
-                { text: 'Web', link: '/web/' }
-              ] 
-            },
-            { text: '关于', link: '/about/' },
-            { text: 'Github', link: 'https://www.github.com/codeteenager' },
-        ],
-    }
+    description: 'My vuepress blog',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ]
 }
