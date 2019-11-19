61ff699040727294d0517f570c8211cd379a4ab9
Taylor <c1171350132@gmail.com>
2019-11-19 17:28:35 +0800

    component

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,18 @@ module.exports = {
     base: '/Vuepress_blog/',
     title: 'Vuepress_blog',
     description: 'My vuepress blog'
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+              items: [
+                { text: 'Android', link: '/android/' },
+                { text: 'ios', link: '/ios/' },
+                { text: 'Web', link: '/web/' }
+              ] 
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+    }
 }
---
+++ docs/.vuepress/theme/components/Wechat.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wechat">
+    <div
+      class="item"
+      v-for="wx in $themeConfig.wxConfig">
+      <h6 class="title" v-show="!notName">{{wx.title}}</h6>
+      <div class="thumb">
+        <img :src="wx.src" :alt="wx.title">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'wechat',
+  props: ['notName']
+}
+</script>
+
+<style scoped lang="stylus">
+.wechat 
+  position fixed;
+  z-index 99;
+  top 90px;
+  right 20px;
+  width 100px;
+
+
+@media (max-width: 1270px)
+  .wechat 
+    display none;
+
+.item 
+  text-align center;
+
+
+img 
+  max-width 80px;
+
+
+h6 
+  margin .4rem;
+
+</style>
