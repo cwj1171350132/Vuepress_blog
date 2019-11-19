805ee9e62887d3683719d2b6097a2aab7f13660e
Jack_Chan <1171350132@qq.com>
2019-11-19 23:07:31 +0800

    inti

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Vuepress_blog
+
+> my blog
+
+## Development
+
+```bash
+yarn dev
+yarn build
+```
+
+For more details, please head VuePress's [documentation](https://v1.vuepress.vuejs.org/).
+
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,70 @@
+const { description } = require('../../package')
+
 module.exports = {
-    base: '/Vuepress_blog/',
-    title: 'Vuepress_blog',
-    description: 'My vuepress blog',
-    head: [
-        ['link', { rel: 'icon', href: '/img/logo.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ]
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Jack_Chan blog',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  base:'/Vuepress_blog',
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '主页',
+        link: '/',
+      },
+      {
+        text: '博客',
+        link: '/article/',
+        items: [ 
+          { text: 'javascript', link: '/article/javascript' },
+          { text: 'linux', link: '/article/linux' },
+        ]
+      },
+    ],
+    sidebar: {
+      '/article/': [
+        {
+          title: '博客',
+          collapsable: false,
+          children: [
+            'javascript'
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
 }
